467e9f9a1a1e0386adb8c52ac3e8af5ec447df1a
pirmanabdurohman <pirman@viztech.com.sg>
2021-04-29 18:06:21 +0800

    fix navbar

--- resources/views/pages/surat/dashboard.blade.php
+++ resources/views/pages/surat/dashboard.blade.php
@@ -6,7 +6,7 @@
         <div class="card mb-5">
             <div class="row card-header">
                 <div class="col-md-6">
-                    Pengajuan dan Permohonan
+                    Layanan Pengajuan dan Permohonan bagi WNI
                 </div>
             </div>
             <div class="card-body">
@@ -177,22 +177,18 @@
                             </div>
                         </a>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-5">
+            <div class="row card-header">
+                <div class="col-md-6">
+                    Layanan Umum
+                </div>
+            </div>
+            <div class="card-body">
 
-                    <div class="col-md-6 col-lg-4 column">
-                        <a href="{{route('legalisir.index')}}" style="text-decoration: none">
-                            <div class="card legalisir">
-                                <div class="txt">
-                                    <h1>PENGAJUAN </br>
-
-                                        LEGALISIR DOKUMEN</h1>
-                                    <p>
-                                        Digunakan untuk melakukan lapor diri jika Anda
-                                        pulang dan menetap kembali di Indonesia
-                                    </p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+                <div class="row">
 
                     <div class="col-md-6 col-lg-4 column">
                         <a href="{{route('akta-lahir.index')}}" style="text-decoration: none">
@@ -201,6 +197,21 @@
                                     <h1>PENGAJUAN </br>
 
                                         AKTA KELAHIRAN</h1>
+                                    <p>
+                                        Digunakan untuk melakukan lapor diri jika Anda
+                                        pulang dan menetap kembali di Indonesia
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-4 column">
+                        <a href="{{route('legalisir.index')}}" style="text-decoration: none">
+                            <div class="card legalisir">
+                                <div class="txt">
+                                    <h1>PENGAJUAN </br>
+
+                                        LEGALISIR DOKUMEN</h1>
                                     <p>
                                         Digunakan untuk melakukan lapor diri jika Anda
                                         pulang dan menetap kembali di Indonesia
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,18 +2,9 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <!-- Users, Roles, Permissions -->
-<li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> User Management</a>
-    <ul class="nav-dropdown-items">
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
-        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('biodata') }}'><i class='nav-icon la la-user-secret'></i> Biodata</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
 
-    </ul>
-</li>
 <li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-hand-paper"></i> Permohonan</a>
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-server"></i> Layanan WNI</a>
     <ul class="nav-dropdown-items">
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('izin-tinggal') }}'><i class='nav-icon la la-home'></i> Izin Tinggal</a></li>
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('pengampunan') }}'><i class='nav-icon la la-hammer'></i> Pengampunan</a></li>
@@ -25,7 +16,20 @@
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tidak-keluar-negeri') }}'><i class='nav-icon la la-outdent'></i> Tidak Keluar Negeri</a></li>
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('kepentingan') }}'><i class='nav-icon la la-warning'></i> Kepentingan</a></li>
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('keterangan-lahir') }}'><i class='nav-icon la la-baby'></i> Keterangan Lahir</a></li>
+
+    </ul>
+</li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-graduation-cap"></i> Layanan Mahasiswa</a>
+    <ul class="nav-dropdown-items">
+
+    </ul>
+</li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-globe"></i> Layanan Umum</a>
+    <ul class="nav-dropdown-items">
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('legalisir') }}'><i class='nav-icon la la-print'></i> Legalisir</a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('akta-lahir') }}'><i class='nav-icon la la-baby-carriage'></i> Akta Lahir</a></li>
 
     </ul>
 </li>
@@ -34,26 +38,16 @@
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-hands-helping"></i> Miscellaneous</a>
     <ul class="nav-dropdown-items">
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tandatangan') }}'><i class='nav-icon la la-signature'></i> Penanda Tangan</a></li>
-
     </ul>
 
 </li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> User Management</a>
+    <ul class="nav-dropdown-items">
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> <span>Users</span></a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('biodata') }}'><i class='nav-icon la la-user-secret'></i> Biodata</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
+{{--        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>--}}
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('alamatindonesia') }}'><i class='nav-icon la la-question'></i> AlamatIndonesias</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('alamatmesir') }}'><i class='nav-icon la la-question'></i> AlamatMesirs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('biodata') }}'><i class='nav-icon la la-question'></i> Biodatas</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('izintinggal') }}'><i class='nav-icon la la-question'></i> IzinTinggals</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('kepentingan') }}'><i class='nav-icon la la-question'></i> Kepentingans</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('keteranganlahir') }}'><i class='nav-icon la la-question'></i> KeteranganLahirs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('masukmesir') }}'><i class='nav-icon la la-question'></i> MasukMesirs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('pendidikanmesir') }}'><i class='nav-icon la la-question'></i> PendidikanMesirs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('pengampunan') }}'><i class='nav-icon la la-question'></i> Pengampunans</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('riwayatpendidikan') }}'><i class='nav-icon la la-question'></i> RiwayatPendidikans</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tandatangan') }}'><i class='nav-icon la la-question'></i> TandaTangans</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tidakkeluarnegeri') }}'><i class='nav-icon la la-question'></i> TidakKeluarNegeris</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('visahaji') }}'><i class='nav-icon la la-question'></i> VisaHajis</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('visaumroh') }}'><i class='nav-icon la la-question'></i> VisaUmrohs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('wilayah') }}'><i class='nav-icon la la-question'></i> Wilayahs</a></li>
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('aktalahir') }}'><i class='nav-icon la la-question'></i> AktaLahirs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('legalisir') }}'><i class='nav-icon la la-question'></i> Legalisirs</a></li>
+    </ul>
+</li>
