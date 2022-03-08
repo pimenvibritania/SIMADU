d821911c21f3e89f3399898682a62f48c2cb8a6e
khoiry2010 <khoiry2010@gmail.com>
2022-03-08 21:53:14 +0700

    refactor DB

---
+++ app/Http/Controllers/StrataController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Strata;
+use Illuminate\Http\Request;
+
+class StrataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Strata  $strata
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Strata $strata)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Strata  $strata
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Strata $strata)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Strata  $strata
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Strata $strata)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Strata  $strata
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Strata $strata)
+    {
+        //
+    }
+}
